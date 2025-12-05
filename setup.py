cedb0ada3feafdddaf3b13f781c7d716d35b9488
mar3hc <tri.maiminh@vn.bosch.com>
2025-12-05 17:14:15 +0700

    migrate from setup.py to pyproject.toml

--- setup.py
+++ setup.py
@@ -183,37 +183,17 @@ if ( ('install' in listCmdArgs) or ('build' in listCmdArgs) or ('sdist' in listC
 # --------------------------------------------------------------------------------------------------------------
 
 # -- the 'setup' itself
+# NOTE: Metadata now comes from pyproject.toml
+# This minimal setup.py only runs custom build steps and delegates to pyproject.toml
 
-print(COLBY + "Extended setup step 5/5: install.run(self)")
+print(COLBY + "Extended setup step 5/5: Running setup with pyproject.toml configuration")
 print()
 
 setuptools.setup(
-    name         = str(oRepositoryConfig.Get('REPOSITORYNAME')),
-    version      = str(oRepositoryConfig.Get('PACKAGEVERSION')),
-    author       = str(oRepositoryConfig.Get('AUTHOR')),
-    author_email = str(oRepositoryConfig.Get('AUTHOREMAIL')),
-    description  = str(oRepositoryConfig.Get('DESCRIPTION')),
-    long_description = long_description,
-    long_description_content_type = str(oRepositoryConfig.Get('LONGDESCRIPTIONCONTENTTYPE')),
-    url = str(oRepositoryConfig.Get('URL')),
-    packages = [str(oRepositoryConfig.Get('PACKAGENAME')),],
-    package_dir = {str(oRepositoryConfig.Get('REPOSITORYNAME')) : str(oRepositoryConfig.Get('PACKAGENAME'))},
-    classifiers = [
-        str(oRepositoryConfig.Get('PROGRAMMINGLANGUAGE')),
-        str(oRepositoryConfig.Get('LICENCE')),
-        str(oRepositoryConfig.Get('OPERATINGSYSTEM')),
-        str(oRepositoryConfig.Get('DEVELOPMENTSTATUS')),
-        str(oRepositoryConfig.Get('INTENDEDAUDIENCE')),
-        str(oRepositoryConfig.Get('TOPIC')),
-    ],
-    python_requires = str(oRepositoryConfig.Get('PYTHONREQUIRES')),
     cmdclass={
         'install': ExtendedInstallCommand,
     },
-    install_requires = oRepositoryConfig.Get('INSTALLREQUIRES'),
-    package_data={f"{oRepositoryConfig.Get('PACKAGENAME')}" : oRepositoryConfig.Get('PACKAGEDATA')},
 )
-
 # --------------------------------------------------------------------------------------------------------------
 
 print()
